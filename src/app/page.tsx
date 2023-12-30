564a18fd7a55e124a3bfc6e304b7d303059d2c3c
David Qing <davidqing6432@gmail.com>
2023-12-29 17:49:58 -0800

    introduce index page, split student and ta, existing issues with upload assignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import Image from 'next/image';
+import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <>
-      <h1>WDD PORTAL</h1>
-    </>
-  )
+    return (
+        <div className={styles.container}>
+            <h1>Welcome to the WDD Portal.</h1>
+            <Link href="student">For students</Link>
+            <Link href="ta">For TAs</Link>
+        </div>
+    );
 }
---
+++ src/app/ta/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function TAIndex() {
+    return (
+        <div>
+            <h1>TA Portal</h1>
+            <Link href="ta/upload-assignment">Uplaod a new assignment</Link>
+        </div>
+    );
+}
